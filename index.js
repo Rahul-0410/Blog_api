9c14e4ab36019e1217b7e9116351b36eaafe9ba8
Rahul-0410 <rgs786999@gmail.com>
2024-09-04 22:10:23 +0530

    login and signup

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const app=express();
+const bcrypt = require('bcrypt');
 const User= require('./model/user');
-app.use(cors());
+const jwt=require('jsonwebtoken');
+
+// to pass cookie in react set credentials to include
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
+
+
+const salt=bcrypt.genSaltSync(10);
+
+//for jwt
+const secret= 'yugdweud2378e2387eh2';
 
 mongoose.connect('mongodb://localhost:27017/auth',{
     
@@ -14,10 +24,28 @@ mongoose.connect('mongodb://localhost:27017/auth',{
 app.post('/register',async (req,res)=>{
     const {username,password}=req.body;
     try{
-        const UserDoc= await User.create({username,password});
+        const UserDoc= await User.create({username,
+            password:bcrypt.hashSync(password,salt)});
         res.json(UserDoc);
     }catch(err){
         res.status(400).json({message:err.message});
+    }
+})
+app.post('/login',async (req,res)=>{
+    const {username,password}=req.body;
+    const userDoc= await User.findOne({username})
+   const passOk= bcrypt.compareSync(password,userDoc.password);
+    // res.json(passOk);
+    if(passOk){
+        //logged in
+        jwt.sign({username,id:userDoc._id}, secret, {},(err,token)=>{
+                if(err){
+                    res.status(400).json({message:'login failed'});
+                }
+                res.cookie('token',token).json('ok');
+        })
+    } else{
+        res.status(400).json({message:'login failed'});
     }
 })
 
